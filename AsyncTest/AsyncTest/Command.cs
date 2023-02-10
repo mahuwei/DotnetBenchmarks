8e3ef71631940d74d69fbed6f15ee294ffb55566
mahuwei <mahuwei@qq.com>
2023-02-10 17:49:26 +0800

    准备新测试。

---
+++ AsyncTest/AsyncTest/Command.cs
@@ -0,0 +1,64 @@
+﻿namespace AsyncTest;
+
+public delegate void CommandDelegate(string value);
+
+public sealed class Command : IDisposable {
+    public void Dispose() {
+    }
+
+    public event CommandDelegate? OnCommandDelegate;
+
+    public void GetValueEventAsync() {
+        var tk = new CancellationTokenSource(new TimeSpan(0, 0, 0, 3));
+        ValueEvent(tk.Token);
+    }
+
+    private void ValueEvent(CancellationToken cancellationToken, int sleepSecond = 2) {
+        Console.WriteLine("  start call ValueEventAsync.");
+        Task.Run(async () => {
+            await Task.Delay(sleepSecond * 1000, cancellationToken);
+            OnCommandDelegate?.Invoke($"GetValueEventAsync:{DateTime.Now}");
+        }, cancellationToken);
+        Console.WriteLine("  call ValueEventAsync end.");
+    }
+
+    public async Task<string?> GetValueTaskAsync() {
+        var tk = new CancellationTokenSource(new TimeSpan(0, 0, 0, 5));
+        string? resultValue = null;
+        var isComplete = false;
+        // 定义一个事件处理的内部方法，以便于取消事件订阅。
+        void OnOnCommandDelegateHandler(string value) {
+            Console.WriteLine($"  OnCommandDelegate事件返回结果。{value}");
+            resultValue = value;
+            isComplete = true;
+        }
+
+        OnCommandDelegate += OnOnCommandDelegateHandler;
+
+        try{
+            var task = Task.Run(() => {
+                do{
+                    if(isComplete == false){
+                        Thread.Sleep(100);
+                        continue;
+                    }
+
+                    break;
+                } while(true);
+
+                return resultValue;
+            }, tk.Token);
+            ValueEvent(tk.Token, 6);
+            var waitAsync = await task.WaitAsync(tk.Token);
+            task.Dispose();
+            return waitAsync;
+        }
+        catch(Exception ex){
+            Console.WriteLine(ex);
+            throw;
+        }
+        finally{
+            OnCommandDelegate -= OnOnCommandDelegateHandler;
+        }
+    }
+}
---
+++ AsyncTest/AsyncTest/Program.cs
@@ -0,0 +1,44 @@
+﻿using AsyncTest;
+
+Console.Title = "异步转同步";
+Console.WriteLine("按任意键退出程序...");
+Console.WriteLine();
+var command = new Command();
+try{
+    Console.WriteLine("开始调用：GetValueEventAsync");
+    command.OnCommandDelegate += OnHandler;
+    command.GetValueEventAsync();
+}
+catch(Exception ex){
+    Console.WriteLine($"  错误信息:{ex.Message}");
+}
+finally{
+    await Task.Delay(5000);
+    command.OnCommandDelegate -= OnHandler;
+    command.Dispose();
+}
+
+Console.WriteLine();
+command = new Command();
+try{
+    Console.WriteLine("开始调用：GetValueTaskAsync");
+    var valueTaskAsync = await command.GetValueTaskAsync();
+    Console.WriteLine($"  调用 GetValueTaskAsync 后，返回结果：{valueTaskAsync}");
+}
+catch(Exception ex){
+    Console.WriteLine($"  错误信息:{ex.Message}");
+}
+
+await Task.Delay(5000);
+command.GetValueEventAsync();
+command.OnCommandDelegate += OnHandler;
+
+await Task.Delay(5000);
+command.OnCommandDelegate -= OnHandler;
+command.Dispose();
+
+Console.ReadKey();
+
+void OnHandler(string value) {
+    Console.WriteLine($"  调用 GetValueEventAsync 后，通过事件返回结果。{value}");
+}
